c59e09ac675a9a1ae824f3f350df176337953100
nguyentuan1696 <nguyentuan1696@gmail.com>
2024-07-31 10:15:56 +0700

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className='w-full border-t py-2'>
-      <p className='text-center	'>footer</p>
+      <p className='text-center	'>Tuan Nguyen</p>
     </footer>
   )
 }
